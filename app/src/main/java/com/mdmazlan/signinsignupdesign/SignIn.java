2f966f7dfc3973090bdf02a2d93b4c4bea3c7aa0
MAZLAN <mdmazlan9@gmail.com>
2021-05-05 09:59:29 +0600

    Last Update

--- app/src/main/java/com/mdmazlan/signinsignupdesign/SignIn.java
+++ app/src/main/java/com/mdmazlan/signinsignupdesign/SignIn.java
@@ -2,13 +2,31 @@ package com.mdmazlan.signinsignupdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
 
+
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        textView = findViewById(R.id.txt_sign_up);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this,SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/mdmazlan/signinsignupdesign/SignUp.java
+++ app/src/main/java/com/mdmazlan/signinsignupdesign/SignUp.java
@@ -2,13 +2,33 @@ package com.mdmazlan.signinsignupdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        textView = findViewById(R.id.txt_sign_in);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this,SignIn.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
 }
